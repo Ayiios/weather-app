06f9f5d92c90038100740598ade04c8fedf02380
Aymen osman <aymeeen.osman@hotmail.se>
2022-04-09 20:49:13 +0200

    Byggt upp mapp struktur

---
+++ src/navigation/Main.jsx
@@ -0,0 +1,24 @@
+// import { Route, Routes } from "react-router-dom";
+// import Home from '../components/views/Home';
+// import About from '../components/views/About';
+
+
+
+// const Main = () => {
+
+
+//     return(<Routes>
+
+       
+//        <Route exact-path="/"><Home/></Route>
+//        <Route path="/about"><About/></Route>
+
+
+
+
+
+//     </Routes>)
+
+
+
+// };
