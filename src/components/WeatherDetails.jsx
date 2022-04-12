9520639781edd103cb0d808c67f8125a6b3e6269
Aymen osman <aymeeen.osman@hotmail.se>
2022-04-12 17:49:16 +0200

    weather details små fix

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,13 +1,19 @@
 import { useParams } from "react-router-dom"
-//import WeatherCard from "./WeatherCard"
 import useFetch from "./hooks/useFetch"
-//import WeatherCard from "./WeatherCard";
+
 
 const WeatherDetails = () => {
 
     const {location}= useParams()
+    
+   
+    
 
     var url = `http://api.weatherapi.com/v1/forecast.json?key=6c5515a185a943bc88c92724220404&q=${location}&days=5&aqi=no&alerts=no`;
+
+   
+    
+
 
     console.log(location)
 
@@ -22,7 +28,11 @@ const WeatherDetails = () => {
         <h3>Text i min weather details</h3>
         {data && (<div>
             <article>
-            <h2>{data.location.name}</h2> 
+        
+           { <h1>{data.location.name}</h1>}
+    <p>{data.current.temp_c}</p>
+     <p>{data.forecast.forecastday[0].date}</p>
+      <img src={data.current.condition.icon} alt="symbol" />
 
 
             </article>
